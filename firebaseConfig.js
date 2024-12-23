340686e6d6b1c73f6382a60ec4160971f5bd3700
ValhallaAMB <you@example.com>
2024-12-23 16:14:25 +0300

    Added all auth functions, renamed chats.tsx and moved chatroom.tsx's location

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBiARBOkVkFY79sJTP5GA6slT25dnSvaI",
+  authDomain: "altarya-571ed.firebaseapp.com",
+  projectId: "altarya-571ed",
+  storageBucket: "altarya-571ed.firebasestorage.app",
+  messagingSenderId: "352050324209",
+  appId: "1:352050324209:web:fb2b421ec6f7619692bf50"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const usersCollection = collection(db, "users");
+export const roomsCollection = collection(db, "rooms");
